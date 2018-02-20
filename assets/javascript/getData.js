5801c0ac22f64c5a26e7959f0fb13954322c0bad
ramantehlan <ramantehlan@gmail.com>
2018-02-21 00:53:06 +0530

    Minor changes

--- assets/javascript/getData.js
+++ assets/javascript/getData.js
@@ -20,70 +20,136 @@ function createElement(type, attribute = null, content = null, child = null) {
 
   // add child by appending them
   $.each(child, function(key, value) {
-      element.appendChild(value)
+    element.appendChild(value)
   })
 
   // return back the element
   return element
 }
 
-function createLinks(links){
+function createLinks(links) {
   let stack = [];
 
-  $.each(links, function(key, value){
-      if(links[key].active == true){
-        let finalForm = []
-        let content = links[key].label
+  $.each(links, function(key, value) {
+    if (links[key].active == true) {
+      let finalForm = []
+      let content = links[key].label
 
-        finalForm.push(createElement("img", {
-          "src": links[key].iconUrl,
-          "alt": links[key].name
-        }))
+      finalForm.push(createElement("img", {
+        "src": links[key].iconUrl,
+        "alt": links[key].name
+      }))
 
-        if(links[key].url != ""){
-          finalForm.push(createElement("a", {
-            "href": links[key].url
-          }, links[key].label))
-          content = null
-        }
-
-        stack.push(createElement("div",{
-            "class" : "link"
-        }, content, finalForm))
+      if (links[key].url != "") {
+        finalForm.push(createElement("a", {
+          "href": links[key].url
+        }, links[key].label))
+        content = null
       }
+
+      stack.push(createElement("div", {
+        "class": "link"
+      }, content, finalForm))
+    }
   })
 
   return stack;
 }
 
-function createItems(data){
+function createItems(data, locationEnable = true) {
   let item = []
-  $.each(data, function(key, value){
-      let subItems = []
+  $.each(data, function(key, value) {
+    let subItems = []
+    let display = true;
 
-      if(data.hasOwnProperty('url') && value.hasOwnProperty('name')){
+    //iallow display
+    if (value.hasOwnProperty("complete")) {
+      //if complete is false
+      if (!value.complete) {
+        display = false;
+      }
+    }
+
+    if (display) {
+
+      //heading
+      if (data.hasOwnProperty('url') && value.hasOwnProperty('name')) {
         subItems.push(createElement("div", {
           "class": "item_heading"
-        }, null , [createElement("a", {"href": value.url}, value.name)]))
-      }else if (value.hasOwnProperty('name')) {
+        }, null, [createElement("a", {
+          "href": value.url
+        }, value.name)]))
+      } else if (value.hasOwnProperty('name')) {
         subItems.push(createElement("div", {
           "class": "item_heading"
-        }, value.name ))
+        }, value.name))
       }
 
+      if (data.hasOwnProperty('url') && data.hasOwnProperty('institute')) {
+        subItems.push(createElement("div", {
+          "class": "item_heading"
+        }, null, [createElement("a", {
+          "href": value.url
+        }, value.institute)]))
+      } else if (value.hasOwnProperty('institute')) {
+        subItems.push(createElement("div", {
+          "class": "item_heading"
+        }, value.institute))
+      }
 
-      subItems.push(createElement("div", {
-        "class": "item_subheading"
-      }, value.type))
+      if (data.hasOwnProperty('url') && data.hasOwnProperty('title')) {
+        subItems.push(createElement("div", {
+          "class": "item_heading"
+        }, null, [createElement("a", {
+          "href": value.url
+        }, value.title)]))
+      } else if (value.hasOwnProperty('title')) {
+        subItems.push(createElement("div", {
+          "class": "item_heading"
+        }, value.title))
+      }
 
+      // subheading
+      if (value.hasOwnProperty('type')) {
+        subItems.push(createElement("div", {
+          "class": "item_subheading"
+        }, value.type))
+      }
+
+      if (value.hasOwnProperty('role')) {
+        subItems.push(createElement("div", {
+          "class": "item_subheading"
+        }, value.role))
+      }
+
+      if (value.hasOwnProperty('location') && locationEnable) {
+        subItems.push(createElement("div", {
+          "class": "item_subheading"
+        }, value.location))
+      }
+
+      if (value.hasOwnProperty('level')) {
+        subItems.push(createElement("div", {
+          "class": "item_subheading"
+        }, value.level))
+      }
+
+      if (value.hasOwnProperty('from')) {
+        subItems.push(createElement("div", {
+          "class": "item_subheading"
+        }, value.from))
+      }
+
+      // timeline
       subItems.push(createElement("div", {
         "class": "item_timeline"
       }, value.timeline))
 
-      if(tagsEnable){
+      //tags
+      if (value.hasOwnProperty('tag')) {
         let tags = []
 
-        $.each(value.tag, function(key, value){
+        $.each(value.tag, function(key, value) {
           tags.push(createElement("div", {
             "class": "tag"
           }, value))
@@ -94,24 +160,27 @@ function createItems(data){
         }, null, tags))
       }
 
+      //Details
       subItems.push(createElement("div", {
         "class": "item_details"
       }, value.about))
 
+      //Finishing off
       let workItem = createElement("div", {
         "class": "item"
       }, null, subItems)
 
       item.push(workItem)
+    }
   })
 
   return item;
 }
 
-function pushData(boxes, elements){
+function pushData(boxes, elements) {
   // Adding all the nodes
-  $.each(boxes, function(key1, value1){
-    $.each(elements[key1], function(key2, value2){
+  $.each(boxes, function(key1, value1) {
+    $.each(elements[key1], function(key2, value2) {
       boxes[key1].append(value2);
     });
   });
@@ -132,8 +201,6 @@ function setData(data) {
 
   let profile_elements = [];
   let welcome_elements = [];
-  let work_elements = [];
-  let project_elements = [];
 
   // Minor elements
   let profile_image = createElement("img", {
@@ -159,27 +226,42 @@ function setData(data) {
 
   profile_elements.push(createElement("div", {
     "class": "profile_links"
-  }, null , createLinks(data.profile.link)))
+  }, null, createLinks(data.profile.link)))
 
   // Welcome_elements
   welcome_elements.push(createElement("div", {
-      "class": "super_heading"
+    "class": "super_heading"
   }, data.home.title))
 
   welcome_elements.push(createElement("div", {
-      "class": "super_details"
+    "class": "super_details"
   }, data.home.body))
 
   // Work elelements
-  work_elements.push(createItems(data.work))
+  let work_elements = createItems(data.work, false)
 
   //project elements
-  project_elements.push(createItems(data.projects))
+  let project_elements = createItems(data.projects)
 
+  //achievements elements
+  let achievement_elements = createItems(data.achievements)
 
   // store boxes and respactive data
-  let boxes = [$("#profile_box"), $("#welcome_section"), $("#work_section"), $("#projects_section")];
-  let elements = [profile_elements, welcome_elements, work_elements, project_elements];
+  let boxes = [
+    $("#profile_box"),
+    $("#welcome_section"),
+    $("#work_section"),
+    $("#projects_section"),
+    $("#achievements_section")
+  ];
+
+  let elements = [
+    profile_elements,
+    welcome_elements,
+    work_elements,
+    project_elements,
+    achievement_elements
+  ];
 
   // To push it to html page
   pushData(boxes, elements);
